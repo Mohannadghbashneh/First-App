276d2f1aa42001ec5fe5cb7fdccb3b60d46439a7
mohannadghbashneh <ghbashneh99@gmail.com>
2022-08-21 19:31:25 +0300

    lab1

---
+++ src/components/HornedBeast.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class HornedBeast extends React.Component{
+    render(){
+return(
+    <div>
+        <h2>dog</h2>
+        <img src=''>dog</img>
+        <p> Dog name sasha</p>
+    </div>
+)
+    }
+}
+export default HornedBeast ;

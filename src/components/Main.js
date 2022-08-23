bd0844351229c22a9a522ba2b81fe0b5e7b72538
mohannadghbashneh <ghbashneh99@gmail.com>
2022-08-23 15:26:53 +0300

    edit02

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import HornedBeast from './HornedBeast';
+import React from "react";
+import HornedBeast from "./HornedBeast";
+import data from "./Json.json";
+import {Row} from "react-bootstrap";
 
+
+let info = data.map(element => 
+    <HornedBeast 
+        id = {element._id}
+        title = {element.title}
+        img = {element.image_url}
+        des = {element.description}
+    /> 
+);
 
 class Main extends React.Component {
-  render(){
-    return(
-    <div>
-      <HornedBeast/> 
-
-    </div>
-    )
-  }
+    render(){
+        return(
+          <Row xs={2} md={3} className="g-4">
+            {info}
+          </Row>
+        )
+    }
 }
 
 export default Main;

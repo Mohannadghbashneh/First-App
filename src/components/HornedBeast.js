4e56e6c027af23e40240d11a7b2083e4d42bbe26
mohannadghbashneh <ghbashneh99@gmail.com>
2022-08-23 19:03:57 +0300

    03

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,35 +1,109 @@
+// import React from "react";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import Card from "react-bootstrap/Card";
+// import {Col} from "react-bootstrap";
+// import Button from 'react-bootstrap/Button';
+
+// class HornedBeast extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       val: "",
+//     };
+//   }
+
+//   vote = () => {
+//     this.setState({
+//       val: this.state.val + "❤️",
+//     });
+//   };
+
+
+
+
+
+//   render() {
+//     return (
+//       <Col>
+//       <Card style={{ width: "18rem", height: "500px"  }}>
+//         <Card.Body>
+//           <Card.Title>{this.props.title}</Card.Title>
+//           <Card.Img
+    
+//             src={this.props.img}
+//             onClick={this.vote}
+//           />
+//           <Card.Text>{this.props.des}</Card.Text>
+//           <Card.Text>Votes: {this.state.val}</Card.Text>
+
+
+ 
+
+        
+        
+        
+//         </Card.Body>
+//       </Card>
+//       </Col>
+
+
+
+
+//     );
+//   }
+// }
+
+// export default HornedBeast;
+
+
+
 import React from "react";
+import Placeholder from 'react-bootstrap/Placeholder';
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import Card from "react-bootstrap/Card";
 import {Col} from "react-bootstrap";
+import Button from 'react-bootstrap/Button';
 
 class HornedBeast extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      val: "",
+      votes: "",
     };
   }
 
-  vote = () => {
+  voting = () => {
     this.setState({
-      val: this.state.val + "❤️",
+      votes: this.state.votes + "❤️",
     });
+    
+    
   };
+  seeMe = () => {
+    this.props.showModal(this.props.title);
+  }
 
   render() {
     return (
+     
       <Col>
-      <Card style={{ width: "18rem", height: "500px"  }}>
+            <Placeholder xs={12} bg="primary" />
+
+    <Card style={{ width: "18rem", height: "600px"  }}>
         <Card.Body>
           <Card.Title>{this.props.title}</Card.Title>
           <Card.Img
-    
+            variant="top"
             src={this.props.img}
-            onClick={this.vote}
-          />
-          <Card.Text>{this.props.des}</Card.Text>
-          <Card.Text>Votes: {this.state.val}</Card.Text>
+             onClick={this.seeMe}
+              
+
+         />
+          <Card.Text>{this.props.description}</Card.Text>
+          <Card.Text>Votes: {this.state.votes}</Card.Text>
+          <Button variant="danger" onClick={this.voting} >Vote for me!</Button>
+
         </Card.Body>
       </Card>
       </Col>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,26 +1,62 @@
+// import React from "react";
+// import HornedBeast from "./HornedBeast";
+// import data from "./Json.json";
+// import {Row} from "react-bootstrap";
+
+
+// let info = data.map(element => 
+//     <HornedBeast 
+//         id = {element._id}
+//         title = {element.title}
+//         img = {element.image_url}
+//         des = {element.description}
+//     /> 
+// );
+
+// class Main extends React.Component {
+//     render(){
+//         return(
+//           <Row xs={2} md={3} className="g-4">
+//             {info}
+//           </Row>
+//         )
+//     }
+// }
+
+// export default Main;
+
+
+
+
 import React from "react";
 import HornedBeast from "./HornedBeast";
-import data from "./Json.json";
+// import data from "./data";
 import {Row} from "react-bootstrap";
 
 
-let info = data.map(element => 
-    <HornedBeast 
-        id = {element._id}
-        title = {element.title}
-        img = {element.image_url}
-        des = {element.description}
-    /> 
-);
 
 class Main extends React.Component {
     render(){
         return(
-          <Row xs={2} md={3} className="g-4">
-            {info}
+          <Row xs={2} md={4} className="g-4">
+            {this.props.beasts.map((beast, index) => {
+            return (
+              <div key={index}>
+                <HornedBeast
+                  showModal = {this.props.showModal}
+                  title={beast.title}
+                  img={beast.image_url}
+                  description={beast.description}
+                  horns={beast.horns}
+                />
+              </div>
+            )
+          })}
           </Row>
         )
     }
 }
 
 export default Main;
+
+

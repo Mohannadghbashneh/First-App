2fdb94c44ad84153e0c7d60aa0360bf0cc3cb053
mohannadghbashneh <ghbashneh99@gmail.com>
2022-08-23 19:06:52 +0300

    lab03

--- src/App.js
+++ src/App.js
@@ -1,26 +1,3 @@
-// import React from 'react';
-// import Header from './components/Header';
-// import Footer from './components/Footer';
-// import Main from './components/Main';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
-// class App extends React.Component {
-//   render(){
-//     return(
-//     <div>
-//       <Header />
-//       <Main />
-//       <Footer />
-
-//     </div>
-//     )
-//   }
-// }
-
-// export default App;
-
-
-
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,36 +1,5 @@
-// import React from "react";
-// import HornedBeast from "./HornedBeast";
-// import data from "./Json.json";
-// import {Row} from "react-bootstrap";
-
-
-// let info = data.map(element => 
-//     <HornedBeast 
-//         id = {element._id}
-//         title = {element.title}
-//         img = {element.image_url}
-//         des = {element.description}
-//     /> 
-// );
-
-// class Main extends React.Component {
-//     render(){
-//         return(
-//           <Row xs={2} md={3} className="g-4">
-//             {info}
-//           </Row>
-//         )
-//     }
-// }
-
-// export default Main;
-
-
-
-
 import React from "react";
 import HornedBeast from "./HornedBeast";
-// import data from "./data";
 import {Row} from "react-bootstrap";
 
 

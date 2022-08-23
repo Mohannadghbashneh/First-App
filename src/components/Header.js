f9ec2e9d09689f688eebbdc3aa388ee978f0d6f9
mohannadghbashneh <ghbashneh99@gmail.com>
2022-08-23 19:25:34 +0300

    LAB03

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,26 +19,17 @@ class Header extends React.Component {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            
+            <NavDropdown title="OUR SOCIAL MAEDIA" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">FACEBOOK 👤</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">INSTAGRAM 📷</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+              
             </NavDropdown>
           </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
+         
+            
+          
         </Navbar.Collapse>
       </Container>
     </Navbar>
